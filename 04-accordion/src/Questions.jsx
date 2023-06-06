60042e6042440acc286b8ebcb712b5edd5c3c7c2
Dannizzzzz <oliviazhang096@gmail.com>
2023-06-06 20:13:06 +0930

    only show one info at a time

--- 04-accordion/src/Questions.jsx
+++ 04-accordion/src/Questions.jsx
@@ -1,11 +1,18 @@
 import SingleQuestion from './SingleQuestion';
 
-const Questions = ({ questions }) => {
+const Questions = ({ questions, activeId, toogleQuestion }) => {
   return (
     <section className="container">
       <h1>Questions</h1>
       {questions.map((question) => {
-        return <SingleQuestion key={question.id} {...question} />;
+        return (
+          <SingleQuestion
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toogleQuestion={toogleQuestion}
+          />
+        );
       })}
     </section>
   );
