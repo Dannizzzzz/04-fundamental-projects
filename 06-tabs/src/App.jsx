08f8747fac1e263215f3b63d74b62585c576b8bf
Dannizzzzz <oliviazhang096@gmail.com>
2023-06-08 13:28:44 +0930

    add job buttons

--- 06-tabs/src/App.jsx
+++ 06-tabs/src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import JobInfo from './JobInfo';
+import BtnContainer from './BtnContainer';
 
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(0);
   const fetchJobs = async () => {
     const resp = await fetch(url);
     const newJobs = await resp.json();
@@ -24,7 +26,12 @@ const App = () => {
   }
   return (
     <section className="jobs-center">
-      <JobInfo jobs={jobs} />
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      <JobInfo jobs={jobs} currentItem={currentItem} />
     </section>
   );
 };
