effc81c65fe567f4f641aff6f2437da8441e5d2b
Dannizzzzz <oliviazhang096@gmail.com>
2023-06-08 11:41:21 +0930

    fetch data from url and add loading pattern

--- 06-tabs/src/App.jsx
+++ 06-tabs/src/App.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react';
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const fetchJobs = async () => {
+    const resp = await fetch(url);
+    const newJobs = await resp.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
   return <h2>Tabs Starter</h2>;
 };
 export default App;
